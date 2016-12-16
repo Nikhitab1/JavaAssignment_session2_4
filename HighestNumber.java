315a7ceaa9bc5dfc7405518e53498af38f824064
Nikhitab1 <b.nikhitha.sai@gmail.com>
2016-12-16 20:42:56 +0530

    Add files via upload

---
+++ HighestNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class HighestNumber {
+
+	public static void main(String[] args) {
+		int a=100,b=50,c=90;
+		
+		
+		if(a>b && a>c){
+			System.out.println("a is highest and its value is \n" + a);
+			}
+		else if(b>a && b>c){
+			System.out.println("b is highest and its value is \n" + b);
+		}
+		else 
+			
+			System.out.println("c is highest and its value is \n" + c);
+		}
+
+	}
+
+
